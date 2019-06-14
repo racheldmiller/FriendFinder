834c08f2e6be9512f2cbeb46f81ff8ad594e621c
racheldmiller <rachelmiller415@gmail.com>
2019-06-14 09:30:50 -0700

    adding pseudocode of requirements

---
+++ server.js
@@ -0,0 +1,5 @@
+// Your server.js file should require the basic npm packages we've used in class: express and path.
+
+// Dependencies
+var express = require("express");
+var path = require("path");
